3a7d2d79a1f89a42abf464b7eb7d6fdda99bdb57
aleanvalle <alexv4lle@gmail.com>
2024-02-24 13:17:58 -0300

    agregados Prettier y Permutations

---
+++ Utils/Permutations/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Permutations
+{
+    public class Program
+    {
+        bool IsOdd(int number)
+        {
+            return number % 2 == 1;
+        }
+
+        void Swap<T>(T[] elementsToPermute, int a, int b)
+        {
+            T temp = elementsToPermute[a];
+            elementsToPermute[a] = elementsToPermute[b];
+            elementsToPermute[b] = temp;
+        }
+
+        public T[][] GetPermutations<T>(T[] elementsToPermute, int n)
+        {
+            List<List<T>> permutations = new List<List<T>>();
+            if (n == 1)
+            {
+                permutations.Add(elementsToPermute.ToList());
+                return permutations.Select(l => l.ToArray()).ToArray();
+            }
+            int[] index = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                index[i] = 0;
+            }
+            permutations.Add(elementsToPermute.ToList());
+            for (int i = 1; i < n;)
+            {
+                if (index[i] < i)
+                {
+                    if (IsOdd(i))
+                    {
+                        Swap(elementsToPermute, i, index[i]);
+                    }
+                    else
+                    {
+                        Swap(elementsToPermute, i, 0);
+                    }
+                    permutations.Add(elementsToPermute.ToList());
+                    index[i]++;
+                    i = 1;
+                }
+                else
+                {
+                    index[i++] = 0;
+                }
+            }
+            return permutations.Select(l => l.ToArray()).ToArray();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Utils/Prettier/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Prettier
+{
+    public class Program
+    {
+        public void PrintArray<T>(T[] arr)
+        {
+            int arrLength = arr.Length;
+            Console.Write("[");
+            for (int i = 0; i < arrLength; i++)
+            {
+                Console.Write(string.Format("{0}", arr[i]));
+                if (i + 1 != arrLength)
+                {
+                    Console.Write(", ");
+                }
+                else
+                {
+                    Console.Write("]");
+                }
+            }
+            return;
+        }
+
+        public void PrintMatrix<T>(T[][] arr)
+        {
+            int rowLength = arr.Length;
+            int colLength = arr[0].Length;
+            Console.Write("[");
+            for (int i = 0; i < rowLength; i++)
+            {
+                Console.Write("[");
+                for (int j = 0; j < colLength; j++)
+                {
+                    Console.Write(string.Format("{0}", arr[i][j]));
+                    if (j + 1 != colLength)
+                    {
+                        Console.Write(",");
+                    }
+                    else
+                    {
+                        Console.Write("]");
+                    }
+                }
+                if (i + 1 != rowLength)
+                {
+                    Console.Write("\n");
+                }
+                else
+                {
+                    Console.Write("]");
+                }
+            }
+            return;
+        }
+
+        static void Main(string[] args)
+        {
+            Program a = new Program();
+            string[] b = { "abc", "def", "ghi" };
+            a.PrintArray(b);
+        }
+    }
+}
